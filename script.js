97356253e8309df780e2156716ea45daa6cccba7
pavanireddy2005 <paavanireddy842@gmail.com>
2026-06-13 22:00:24 +0530

    media update

--- script.js
+++ script.js
@@ -46,3 +46,20 @@ form.addEventListener("submit", function (e) {
         console.error("EmailJS Error:", error);
     });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});

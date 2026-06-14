05c9b3f09d1b44294d480ec1f3d5b1188adf5ef7
pavanireddy2005 <paavanireddy842@gmail.com>
2026-06-14 10:26:49 +0530

    ligh- mode

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Theme Toggle
 const toggleBtn = document.getElementById("theme-toggle");
 
 toggleBtn.addEventListener("click", () => {
+
     document.body.classList.toggle("light-mode");
 
-    if (document.body.classList.contains("light-mode")) {
+    if(document.body.classList.contains("light-mode")){
         toggleBtn.textContent = "🌙";
-    } else {
+    }else{
         toggleBtn.textContent = "☀️";
     }
-});
 
+});
 // Initialize EmailJS
 emailjs.init("rCxFnTBnWcm42WNwY");
 
@@ -63,3 +63,4 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     });
 
 });
+
